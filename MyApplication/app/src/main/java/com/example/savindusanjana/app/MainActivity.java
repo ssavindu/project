f52b94e9af9a955449238f78d73a2458626cf426
ssavindu <ssavindu@gmail.com>
2017-08-09 19:45:46 +0530

    Fab and alert dialog added

--- MyApplication/app/src/main/java/com/example/savindusanjana/app/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/savindusanjana/app/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.savindusanjana.app;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void clicked(View v){
+        AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+        alert.setTitle("This is an alert");
+        alert.setMessage("FAB WORKS!!");
+        alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.show();
     }
 }
